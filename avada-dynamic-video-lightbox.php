a8675eeccd2373e34f61b37593c8ff22cb7203a8
4suredev <64234115+4suredev@users.noreply.github.com>
2022-10-12 09:01:19 +0800

    Bug Fixes

--- avada-dynamic-video-lightbox.php
+++ avada-dynamic-video-lightbox.php
@@ -3,12 +3,12 @@
  * Plugin Name: Avada Dynamic Video Lightbox
  * Plugin URI: https://4sure.com.au
  * Description: Insert a youtube or vimeo link that plays in a lightbox. Use [lightbox_video url=""]. Add a custom thumbnail using the 'img' parameter.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: 4sure
  * Author URI: https://4sure.com.au
  */
-define('VBL_PLUGIN_PATH', home_url().'/wp-content/plugins/avada-dynamic-video-lightbox/');
-include_once( plugin_dir_path( __FILE__ ) . 'updater.php');
+define('VBL_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+include_once( VBL_PLUGIN_PATH. 'updater.php');
 $updater = new Avada_dynamic_lightbox_updater( __FILE__ ); 
 $updater->set_username( '4suredev' ); 
 $updater->set_repository( 'Avada-Dynamic-Video-Lightbox' ); 
@@ -30,17 +30,26 @@ function custom_lightbox_video($atts = array()){
     $html = '';
     if( $pos = strpos($args['url'], "youtube.com/watch") ){
         $video_id = strtok(str_replace("watch?v=", "", substr($args['url'], strpos($args['url'], 'watch?v='))), '&');
+        if(strpos($video_id, '?')){
+            $video_id = substr($video_id, 0, strpos($video_id, '?'));
+        }
         if($args['img'] == '') $image = 'https://img.youtube.com/vi/'.$video_id.'/maxresdefault.jpg';
         else $image = $args['img'];
     }
     else if( $pos = strpos($args['url'], "youtube.com/embed") ){
         $video_id = strtok(str_replace("embed/", "", substr($args['url'], strpos($args['url'], 'embed'))), '?');
+        if(strpos($video_id, '?')){
+            $video_id = substr($video_id, 0, strpos($video_id, '?'));
+        }
         if($args['img'] == '') $image = 'https://img.youtube.com/vi/'.$video_id.'/maxresdefault.jpg';
         else $image = $args['img'];
     }
     else if( $pos = strpos($args['url'], "youtu.be") ){
         $video_id = strtok(str_replace("youtu.be/", "", substr($args['url'], strpos($video, 'youtu.be/'))), '&');
         $video_id = str_replace("https://", "", $video_id);
+        if(strpos($video_id, '?')){
+            $video_id = substr($video_id, 0, strpos($video_id, '?'));
+        }
         if($args['img'] == '') $image = 'https://img.youtube.com/vi/'.$video_id.'/maxresdefault.jpg';
         else $image = $args['img'];
     }
@@ -50,7 +59,6 @@ function custom_lightbox_video($atts = array()){
         if($args['img'] == '') $image = $video->thumbnail_url;
         else $image = $args['img'];
     }
-    $html .= '<div class="dynamic-video-lightbox"><a href="'.$args['url'].'" class="video-link-preview" target="lightbox" rel="iLightbox"><i class="fa fa-play" style="font-size: 24px; color: #fff;"></i><div class="thumbnail-wrap"><img class="thumbnail" loading="lazy" src="'.$image.'" height="200" width="100%"></div></a></div>';
+    $html .= '<div class="dynamic-video-lightbox"><a href="'.$args['url'].'" class="video-link-preview" target="lightbox" rel="iLightbox"><i class="fas fa-play" style="font-size: 24px; color: #fff;"></i><div class="thumbnail-wrap"><img class="thumbnail" loading="lazy" src="'.$image.'" height="200" width="100%"></div></a></div>';
     return $html;
-    return 'test';
 }
