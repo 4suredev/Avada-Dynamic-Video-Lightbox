73e9944354b0cc3a9146539aa53e285e3110158c
4suredev <64234115+4suredev@users.noreply.github.com>
2025-06-19 09:30:51 +0800

    Check response before comparing versions

--- updater.php
+++ updater.php
@@ -60,15 +60,14 @@ class Avada_dynamic_lightbox_updater {
 	}
     public function modify_transient( $transient ) {
         if( property_exists( $transient, 'checked') ) { // Check if transient has a checked property
-          if( $transient->checked ) { // Did WordPress check for updates?
-            $checked =  $transient->checked;
+          if( $checked = $transient->checked ) { // Did WordPress check for updates?
             $this->get_repository_info(); // Get the repo info
-            if (!empty($checked[$this->basename])) {
-              $out_of_date = version_compare( $this->github_response['tag_name'], $checked[$this->basename], 'gt' ); // Check if we're out of date
+            if( is_array($this->github_response) && !empty($this->github_response['tag_name']) && !empty($checked[$this->basename]) ) { // Check response
+               $out_of_date = version_compare( $this->github_response['tag_name'], $checked[$this->basename], 'gt' ); // Check if we're out of date
             } else {
               $out_of_date = false;
             }
-            if( $out_of_date != false ) {
+            if( $out_of_date ) {
               $new_files = $this->github_response['zipball_url']; // Get the ZIP
               $slug = current( explode('/', $this->basename ) ); // Create valid slug
               $plugin = array( // setup our plugin info
